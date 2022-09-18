ebc094ac3063f2be5e0e121d288a57372db504f2
Deadpo_Ol <https://github.com/devoind>
2022-09-18 21:15:32 +0300

    docker-сщьÐcompose  run

--- docker_config.py
+++ docker_config.py
@@ -1,1 +1,14 @@
-SQLALCHEMY_DATABASE_URI = "postgresql://flask_app:flask-app_password@pg/flask_app"
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://flask_app:flask-app_password@pg/flask_app"
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
