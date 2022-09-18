f0b475e9c5022ed9c438002f6e97ee561c31a2aa
Deadpo_Ol <https://github.com/devoind>
2022-09-18 22:48:27 +0300

    docker_compose  run4

--- docker_ci_config.py
+++ docker_ci_config.py
@@ -1,1 +1,17 @@
-SQLALCHEMY_DATABASE_URI = "postgresql://$DB_USER:$DB_PASSWORD@pg/$DB_NAME"
+# SQLALCHEMY_DATABASE_URI = "postgresql://$DB_USER:$DB_PASSWORD@pg/$DB_NAME"
+import os
+from typing import List
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DEBUG = False
+SQLALCHEMY_DATABASE_URI = "postgresql://{username}:{password}@{host}:{port}/{db_name}".format(
+    username=os.getenv('POSTGRES_USER'),
+    password=os.getenv('POSTGRES_PASSWORD'),
+    host=os.getenv('POSTGRES_HOST', '127.0.0.1'),
+    port=int(os.getenv('POSTGRES_PORT', 5432)),
+    db_name=os.getenv('POSTGRES_DB')
+)
+SWAGGER_SUPPORTED_SUBMIT_METHODS: List[str] = []
